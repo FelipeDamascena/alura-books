1ec72c2e240499da6f42d0c8f5901b512d8d75c2
Felipe Damascena <felipedamascena.soluevo@repassa.com.br>
2024-09-22 19:19:15 -0300

    up project aluraBooks

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome:'Liderança em design', id: 1, src: livro},
+    {nome:'Mestre do Java', id: 2, src: livro},
+    {nome:'Como ser FullStack', id: 3, src: livro},
+    {nome:'A vida e obra de Felipe Damascena', id: 4, src: livro},
+    {nome:'Buscando o conhecimento infinito', id: 5, src: livro}
+]
